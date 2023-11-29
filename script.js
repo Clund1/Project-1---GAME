b3ad852dc0d6e38d1d189d6ed42946aab08d7926
CHL <Clundquist1@gmail.com>
2023-11-28 21:24:30 -0500

    Base Pseudocode

---
+++ script.js
@@ -0,0 +1,30 @@
+/**---------- PLAN OF ACTION ----------**/
+//Identify and Initialize State Variables
+
+//Code main render() & renderResults() functions
+
+//Code Start button as initial renderer
+
+//Code grid(board) => array function to randomly place Goblins on tiles
+
+//Code function to cover all tiles with shrub img
+
+//Code click event listeners
+
+//Code function to reveal 2 goblins or empty tiles beneath and compare values
+
+//Code to either recover tiles or add +1 to goblinsFound
+
+//Code winCondition
+
+//Code retry button
+
+/**---------- CONSTANTS ----------**/
+
+/**---------- CACHED ELEMENT REFs ----------**/
+
+/**---------- INITIAL STATE VARIABLE ----------**/
+
+/**---------- FUNCTIONS ----------**/
+
+/**---------- EVENT LISTENERS ----------**/
