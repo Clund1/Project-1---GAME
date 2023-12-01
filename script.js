61586fda071099d287779078a37b71f0ca4478f9
CHL <Clundquist1@gmail.com>
2023-11-30 21:07:14 -0500

    js for board array || math for board layout

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
 /**---------- PLAN OF ACTION ----------**/
 //Identify and Initialize State Variables
-const forest = {
+const FORESTLOOKUP = {
     0:'', //empty space
-    1:'tree', //hiding spot
-    2:'goblins1', //goblin_match_pair 1
-    3:'goblins2', //goblin_match_pair 2
-    4:'goblins3', //goblin_match_pair 3
-    5:'goblins4', //goblin_match_pair 4
-    6:'goblins5', //goblin_match_pair 5
-    7:'goblins6', //goblin_match_pair 6
+    1:'brown', //hiding spot
+    2:'green', //goblin_match_pair 1
+    3:'blue', //goblin_match_pair 2
+    4:'red', //goblin_match_pair 3
+    5:'purple', //goblin_match_pair 4
+    6:'turquoise', //goblin_match_pair 5
+    7:'orange' //goblin_match_pair 6
 }
 
+//init correct value
+let correctChoice = 0
+
+//init wrong value
+let wrongChoice = 0
+
 let playerChoice = (choice1, choice2) =>//Player Choice will always contain 2 choices
-    if (choice1 === choice2) {
+    {if (choice1 === choice2){
         return correctChoice++ //if choices are equal return truthy value
-    }else{
+        }else{
         return wrongChoice++ //if choice are different return falsey value
     }
 }
@@ -24,12 +30,6 @@ let winner = (correctChoice === 6)
 
 //If Player makes 3 wrong matches loseCondition runs
 let loser = (wrongChoice === 3)
-
-//init correct value
-let correctChoice = 0
-
-//init wrong value
-let wrongChoice = 0
 
 //init board
 let boardArr= null
@@ -42,19 +42,32 @@ function start() {}
 
 //Code main<divs> into gameboard("forest") using forEach array function
 function renderForest(){
-    boardArr= 
-    [0, 0, 0, 0, 0]
-    [0, 0, 0, 0, 0]
-    [0, 0, 0, 0, 0]
-    [0, 0, 0, 0, 0]
-    [0, 0, 0, 0, 0]
+    boardArr= []
+    for (let i = 0; i < 25; i++) {
+        boardArr.push(Math.floor(Math.random() * 6))
+    }
+    let tiles= document.querySelectorAll(".box")
+
+    tiles.forEach((tile)=>{
+        tile.addEventListener('click',()=>{
+            id = tile.id
+            tileArrPosition = boardArr[id]
+            tile.style.backgroundColor = FORESTLOOKUP[tileArrPosition]
+            console.log ("Forest contains ",boardArr)
+            console.log ("Id is ",id)
+            console.log ("tileArrPosition is ", tileArrPosition)
+            console.log(FORESTLOOKUP[tileArrPosition])
+            console.log(tile)
+        })
+    })
 }
+renderForest()
 
 //Code function to place 6 pairs of goblins and after 10 seconds cover all tiles 
 
 //Code click event listeners
-let choice1 = ()
-let choice2 =()
+let choice1 
+let choice2
 //Code function to reveal 2 goblins or empty tiles beneath and compare values
 
 //Code to either recover tiles or add +1 to goblinsFound
@@ -62,7 +75,6 @@ let choice2 =()
 //Code winCondition
 function winCon() {
 //if value of matches === correctMatch return end game win
-
 }
 
 
@@ -71,7 +83,6 @@ function winCon() {
 /**---------- CACHED ELEMENT REFs ----------**/
 const boardEl = document.querySelectorAll('.box')
 const playButton = document.querySelectorAll('button')
-const timer = document.querySelectorAll('timer')
 /**---------- INITIAL STATE VARIABLE ----------**/
 
 /**---------- FUNCTIONS ----------**/
